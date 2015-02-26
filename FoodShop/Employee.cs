22641ac868744c06d85daa7d0ea6b727e97203f2
kmne68 <kmne68@gmail.com>
2015-02-25 23:44:42 -0600

    Added Customer, Employee and WorkSchedule classes.

---
+++ FoodShop/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FoodShop
+{
+    class Employee
+    {
+        private int employeeID { get; set; }
+        private string employeeLast { get; set; }
+        private string employeeFirst { get; set; }
+        private int positionID { get; set; }
+        private int shiftID { get; set; }
+        private double salary { get; set; }
+
+        // Constructor
+        public Employee (int empID, string lastName, string firstName, int posID, int shift, double pay)
+        {
+            employeeID = empID;
+            lastName = employeeLast;
+            firstName = employeeFirst;
+            positionID = posID;
+            shiftID = shift;
+            salary = pay;
+        }
+    }
+}
---
+++ FoodShop/WorkSchedules.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FoodShop
+{
+    class WorkSchedules
+    {
+        int workScheduleID { get; set; }
+        int storeID { get; set; }
+        int employeeID { get; set; }
+        DateTime workDate { get; set; }
+        DateTime startTime { get; set; }
+        DateTime endTime { get; set; }
+        DateTime clockIn { get; set; }
+        DateTime clockOut { get; set; }
+
+        // Constructor
+        public WorkSchedules (int scheduleID, int store, int empID, DateTime date, DateTime start, DateTime end, DateTime inTime, DateTime outTime)
+        {
+            workScheduleID = scheduleID;
+            storeID = store;
+            employeeID = empID;
+            workDate = date;
+            startTime = start;
+            endTime = end;
+            clockIn = inTime;
+            clockOut = outTime;
+        }
+    }
+}
