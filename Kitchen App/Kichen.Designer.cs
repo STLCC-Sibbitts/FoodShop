7438160d44640591fbbcfbdfb371682b8a4acb24
4tutoralcom <4tutoralcom@gmail.com>
2015-04-16 20:33:13 -0500

    Moved Over Kitchen App

---
+++ Kitchen App/Kichen.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Kitchen_App
+{
+    partial class Kichen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeOrders = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.CompletedOrders = new System.Windows.Forms.TreeView();
+            this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // treeOrders
+            // 
+            this.treeOrders.BackColor = System.Drawing.SystemColors.Window;
+            this.treeOrders.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeOrders.Location = new System.Drawing.Point(12, 39);
+            this.treeOrders.Name = "treeOrders";
+            this.treeOrders.Size = new System.Drawing.Size(393, 465);
+            this.treeOrders.TabIndex = 0;
+            this.treeOrders.DoubleClick += new System.EventHandler(this.treeViewOrder_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 510);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(119, 510);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(101, 33);
+            this.btnSettings.TabIndex = 2;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
+            // CompletedOrders
+            // 
+            this.CompletedOrders.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.CompletedOrders.Location = new System.Drawing.Point(411, 39);
+            this.CompletedOrders.Name = "CompletedOrders";
+            this.CompletedOrders.Size = new System.Drawing.Size(393, 465);
+            this.CompletedOrders.TabIndex = 3;
+            this.CompletedOrders.DoubleClick += new System.EventHandler(this.CompletedOrders_DoubleClick);
+            // 
+            // refreshTimer
+            // 
+            this.refreshTimer.Enabled = true;
+            this.refreshTimer.Interval = 500;
+            this.refreshTimer.Tick += new System.EventHandler(this.refreshTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 555);
+            this.Controls.Add(this.CompletedOrders);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeOrders);
+            this.Name = "Form1";
+            this.Text = "Kitichen Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeOrders;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.TreeView CompletedOrders;
+        private System.Windows.Forms.Timer refreshTimer;
+    }
+}
+
---
+++ Kitchen App/Kichen.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kitchen_App
+{
+    public partial class Kichen : Form
+    {
+        public Kichen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int i = treeOrders.Nodes.Count;
+            TreeNode node = treeOrders.Nodes.Add("Order Name "+rand.Next(1,100));
+            node.Tag = node.Text;
+            node.Text += " | 00:00";
+            CompletedNodes.Add(new List<Boolean>());
+            OrdersTimerLog.Add(DateTime.Now);
+                node.Nodes.Add("Order Item 1");
+                CompletedNodes[i].Add(false);
+                    node.LastNode.Nodes.Add("Removed Ingredient");
+
+                node.Nodes.Add("Order Item 2");
+                CompletedNodes[i].Add(false);
+                    node.LastNode.Nodes.Add("Extra Ingredient");
+                node.Nodes.Add("Order Item 3");
+                CompletedNodes[i].Add(false);
+
+            node.ExpandAll();
+
+        }
+
+        private void treeViewOrder_DoubleClick(object sender, EventArgs e)
+        {
+            TreeNode node = treeOrders.SelectedNode;
+            if (node != null && node.Parent != null && node.Parent.Parent == null)
+            {
+                TreeNode nodesParent = node.Parent;
+                node.ForeColor = Color.Green;
+                CompletedNodes[nodesParent.Index][node.Index] = true;
+                Boolean OrderDone = true;
+                foreach (var nodeChild in CompletedNodes[nodesParent.Index])
+                    OrderDone &= nodeChild;
+                if (OrderDone)
+                {
+
+                    node.Parent.ForeColor = Color.Green;
+                    CompletedOrders.Nodes.Clear();
+                    CompletedOrders.Nodes.Add((TreeNode)node.Parent.Clone());
+                    CompletedOrders.ExpandAll();
+                    AllCompletedOrders.Add((TreeNode)node.Parent.Clone());
+                    AllCompletedOrdersTimerLog.Add(OrdersTimerLog[nodesParent.Index]);
+                    node.Parent.Remove();
+                    CompletedNodes.RemoveAt(nodesParent.Index);
+                    OrdersTimerLog.RemoveAt(nodesParent.Index);
+                }
+
+            }
+            else if (node.Parent == null)
+            {
+                node.Expand();
+            }
+        }
+        private void CompletedOrders_DoubleClick(object sender, EventArgs e)
+        {
+            TreeNode nodeSelected = CompletedOrders.SelectedNode;
+            if (nodeSelected.Parent == null)
+            {
+                nodeSelected.ForeColor = Color.Gray;
+                List<Boolean> newNodes= new List<Boolean>();
+
+                foreach (TreeNode node in nodeSelected.Nodes) 
+                {
+                    newNodes.Add(false);
+                    node.ForeColor = Color.Gray;
+                }
+                CompletedNodes.Insert(0,newNodes);
+
+                treeOrders.Nodes.Insert(0,(TreeNode)nodeSelected.Clone());
+                nodeSelected.Remove();
+
+
+                OrdersTimerLog.Insert(0, AllCompletedOrdersTimerLog[nodeSelected.Index]);
+                AllCompletedOrdersTimerLog.RemoveAt(nodeSelected.Index);
+                
+                treeOrders.Nodes[0].ExpandAll();
+                AllCompletedOrders.RemoveAt(AllCompletedOrders.Count-1);
+                if (AllCompletedOrders.Count > 0)
+                {
+                    CompletedOrders.Nodes.Add(AllCompletedOrders.Last());
+                    CompletedOrders.ExpandAll();
+                }
+            }
+        }
+        private List<List<Boolean>> CompletedNodes = new List<List<Boolean>>();
+        private List<TreeNode> AllCompletedOrders = new List<TreeNode>();
+
+        private List<DateTime> OrdersTimerLog = new List<DateTime>();
+        private List<DateTime> AllCompletedOrdersTimerLog = new List<DateTime>();
+
+        private void refreshTimer_Tick(object sender, EventArgs e)
+        {
+            int TIME_OVER_SEC = 10;
+            int TIME_OVER_MIN = 0;
+
+            foreach(TreeNode node in treeOrders.Nodes)
+            {
+
+                TimeSpan timeDiffrence = DateTime.Now - OrdersTimerLog[node.Index];
+                node.Text = String.Format("{0} | {1}:{2}", node.Tag, timeDiffrence.Minutes.ToString().PadLeft(2, '0'), timeDiffrence.Seconds.ToString().PadLeft(2, '0'));
+                if (timeDiffrence.Seconds >= TIME_OVER_SEC && timeDiffrence.Minutes >= TIME_OVER_MIN)
+                {
+                    node.BackColor = (node.BackColor == Color.Red)?Color.Orange:Color.Red;
+                }
+            }
+
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
