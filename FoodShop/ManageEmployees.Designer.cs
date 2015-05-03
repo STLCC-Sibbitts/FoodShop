1d30e7b33708b4a57348bb8c6df76c0cfa11c04e
kmne68 <kmne68@gmail.com>
2015-05-03 17:27:32 -0500

    Committing/pushing so Joe can sort out some confusion.

--- FoodShop/ManageEmployees.Designer.cs
+++ FoodShop/ManageEmployees.Designer.cs
@@ -334,7 +334,7 @@
             this.btn_delete.TabIndex = 56;
             this.btn_delete.Text = "Delete";
             this.btn_delete.UseVisualStyleBackColor = true;
-            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+   //         this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
             // 
             // pnl_salary
             // 
